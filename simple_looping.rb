b9a9e2cb6548ff7fcf5ca4c8e6c5d5f735c81390
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-27 16:11:16 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
     counter = 0
     puts "Welcome to Flatiron School's Web Development Course!"
     counter += 1
-    if counter >= number_of_times
+    if counter < number_of_times
       break
     end
   end
